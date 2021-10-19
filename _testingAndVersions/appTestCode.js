8c1a634267d219382135eca2aa729cd2b4ec94d8
MarleenBee <marleenbeekman@gmail.com>
2021-10-19 08:10:23 +0200

    update apptestcode with working map

--- _testingAndVersions/appTestCode.js
+++ _testingAndVersions/appTestCode.js
@@ -1,5 +1,6 @@
 const Hass = require('../node_modules/home-assistant-js-websocket');
 const WebSocket = require('ws');
+
 global.WebSocket = WebSocket;
 
 const HA_Attributes = {
@@ -15,6 +16,15 @@ const HA_DATA = {
     'attributes': HA_Attributes
     // throw the rest
 };
+// console.log(HA_Attributes);
+// console.log(HA_DATA);
+
+/* what if you would just paste the informationfrom the subscribed entity (ex sensor.lumi_lumi_weather_humidity) in the HA_data first, like entity_id.
+then the value of entity_id would become sensor.lumi_lumi_weather_humidity
+then go to attributes and like push the values of the attributes into there. Then any unknown attributes wouldn't get thrown.
+Then go to HA_attributes and just paste/push the data from the API array into there.
+
+*/
 const address = 'http://homeassistant.local:8123';
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJlYTIzMDk5YTMyMjE0MGMzOTFlMzUxYWRmNGY0NjVhOSIsImlhdCI6MTYzMjgxMDg1NSwiZXhwIjoxOTQ4MTcwODU1fQ.NHBA_n47UO_7JZu7DU7El7tCBkYR07LLFHPYhtMJNko';
 const SENSOR_ENTITIES_TO_HOMEY_CAPABILITIES_MAP =// type: number
@@ -61,6 +71,18 @@ async function connect() {
 
 async function homeyMapper(entities) {
     console.log('update entities');
-    console.log(entities);
+    // console.log(entities);
+    const HomeyMap = {
+        Device: Object.values(entities) // jesus christ. its a list of keys and values and the first thing of the value is the entity_id, which is the same as string: entity_id ...
+            .forEach(entity => {
+                const deviceName = entity.entity_id;
+                console.log(`${deviceName}`);
+                return deviceName; 
+            }).filter(deviceName => {
+                // console.log(capabilityId);
+                return typeof deviceName === 'string';
+            }),
+    }
+    console.log(HomeyMap);
 }
 connect();
