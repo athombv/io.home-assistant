5e7a81467460c04f5f4b241830737056dec243b0
MarleenBee <marleenbeekman@gmail.com>
2021-10-20 11:51:57 +0200

    Extended class, split homeyconvert func to new class

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const Homey = require('homey');
-
 const Client = require('./lib/connectHome');
-
 const address = 'http://homeassistant.local:8123';
-    // eslint-disable-next-line max-len
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJlYTIzMDk5YTMyMjE0MGMzOTFlMzUxYWRmNGY0NjVhOSIsImlhdCI6MTYzMjgxMDg1NSwiZXhwIjoxOTQ4MTcwODU1fQ.NHBA_n47UO_7JZu7DU7El7tCBkYR07LLFHPYhtMJNko';
 
 class MyApp extends Homey.App {
@@ -29,7 +26,6 @@ class MyApp extends Homey.App {
 }
 
 module.exports = MyApp;
-
 
 /*
 
