b7fa2a525fd62ef09763f8da5d554af108e10e2f
MarleenBee <marleenbeekman@gmail.com>
2021-10-25 13:43:59 +0200

    removed comments

--- app.js
+++ app.js
@@ -26,18 +26,3 @@ class MyApp extends Homey.App {
 }
 
 module.exports = MyApp;
-
-/*
-    const address = 'http://homeassistant.local:8123';
-    // eslint-disable-next-line max-len
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJlYTIzMDk5YTMyMjE0MGMzOTFlMzUxYWRmNGY0NjVhOSIsImlhdCI6MTYzMjgxMDg1NSwiZXhwIjoxOTQ4MTcwODU1fQ.NHBA_n47UO_7JZu7DU7El7tCBkYR07LLFHPYhtMJNko';
-    const instance = new Connect(address, token); //instance is a child class now of connect. This is not really what you want because this function is where the entire connection process should get started
-    instance.authConnection()
-    .then (
-    this.log('succesfully connected'),
-    )
-    .catch(err => {
-      this.log('failed to connect', err);
-    });
-  }
-*/
