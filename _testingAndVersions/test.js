6f85835422d8d79d09087e655fdc6aabe5f33699
MarleenBee <marleenbeekman@gmail.com>
2021-11-08 15:55:42 +0100

    update - little bit of progress

--- _testingAndVersions/test.js
+++ _testingAndVersions/test.js
@@ -8,80 +8,113 @@
 * if a key is equal to one of the entity registries, paste the device class or measurement class in there (later necessary for capabilities)
 * then check all entiity registries to see what their device_id is. Compare the necessary device_ids to ids of the device registry
 */
-const SENSOR_MAP =// type: number
-{
-    // device_class --> capability --> type
-    // the binary_sensor and sensor capabilities need to be split up because the device class can be he same
-    'temperature': 'measure_temperature',
-    'co': 'measure_co',
-    'co2': 'measure_co2',
-    'pm25': 'measure_pm25',
-    'humidity': 'measure_humidity',
-    'pressure': 'measure_pressure',
-    'noise': 'measure_noise',
-    'rain': 'measure_rain',
-    'wind_strength': 'measure_wind_strength',
-    'wind_angle': 'measure_wind_angle',
-    'gust_strength': 'measure_gust_strength',
-    'gust_angle': 'measure_gust_angle',
-    'battery': 'measure_battery',
-    'power': 'measure_power',
-    'voltage': 'measure_voltage',
-    'current': 'measure_current',
-    'luminance': 'measure_luminance',
-    'ultraviolet': 'measure_ultraviolet',
-    'water_flow': 'measure_water',
-    'water': 'measure_water',
-    'energy': 'meter_power'
-};
-
-const endDevices = {name:"dummy", entities: {}};
-
 const file1 = require("./jsons/device_registry.json");
 const file2 = require("./jsons/entity_registry.json");
 const file3 = require("./jsons/subscribeEntities.json");
 
+const SENSOR_MAP =// type: number
+{
+  // device_class --> capability --> type
+  // the binary_sensor and sensor capabilities need to be split up because the device class can be he same
+  'temperature': 'measure_temperature',
+  'co': 'measure_co',
+  'co2': 'measure_co2',
+  'pm25': 'measure_pm25',
+  'humidity': 'measure_humidity',
+  'pressure': 'measure_pressure',
+  'noise': 'measure_noise',
+  'rain': 'measure_rain',
+  'wind_strength': 'measure_wind_strength',
+  'wind_angle': 'measure_wind_angle',
+  'gust_strength': 'measure_gust_strength',
+  'gust_angle': 'measure_gust_angle',
+  'battery': 'measure_battery',
+  'power': 'measure_power',
+  'voltage': 'measure_voltage',
+  'current': 'measure_current',
+  'luminance': 'measure_luminance',
+  'ultraviolet': 'measure_ultraviolet',
+  'water_flow': 'measure_water',
+  'water': 'measure_water',
+  'energy': 'meter_power'
+};
+
+class compDevice {
+  constructor(deviceName) {
+    this.deviceName = deviceName;
+    this.deviceId;
+    this.subEntities = [];
+    this.matchedEntities = [];
+  }
+  display() {
+    console.log("deviceName: ", this.deviceName);
+    console.log("deviceId: ", this.deviceId);
+    console.log("entity_ids: ", this.matchedEntities);
+
+  }
+}
+
+var homeyDevices = [];
+
+
 const deviceRegistry = [...file1];
 const entityRegistry = [...file2];
+const entityData = [];
 //const subscribeEntities = [...file3];
 //const entityid = ['hello'];
-const entityid = Object.keys(file3);
-const homeyDevices = [];
-const devArray = [];
-
+const entityKeys = Object.keys(file3);
 // if device_id === id place the device_id object / block in a new array where id the owners is of
 function test() {
   let i = 0;
- // console.log(deviceRegistry);
+  let buffer = "test";
+  // console.log(deviceRegistry);
   entityRegistry.forEach(entity => { //maps the entity registry
     deviceRegistry.forEach(device => { //maps the device registry
       if (entity.device_id === device.id) { //if the device_id (which the entity is linked to in the registries) equals to the id of the device, this is true
-      if(!devArray.includes(device.name) && device.manufacturer !== "Google Inc." && device.manufacturer !== "Sonos") {
-        devArray.push(device.name);
-      }
-        console.log("match found with device: ", device.name);
-        console.log("with entity: ", entity.entity_id);
-        entityid.forEach(id => {
-          if (entity.entity_id === id) {
-            //console.log('compatible entity found: ', id);
-            //console.log('Name of entity: ', file3[id].attributes.friendly_name);
-            homeyDevices.push({
-              name: file3[id].attributes.friendly_name,
-              group: device.name,
-              data: {
-                id: file3[id].entity_id,
-              },
-            });
-            //console.log('entity: ', moreDevices);
-            // if the group name of the entitymap above equals the device name, you add it to devArray
-            // the push method might not work because once youre here, the entire thing already is done
-          }
-        })
+        if (device.name !== buffer) {
+          homeyDevices[i] = new compDevice(device.name);
+          homeyDevices[i].deviceId = device.id;
+          buffer = device.name;
+          entityRegistry.forEach(entity_ => {
+            if (homeyDevices[i].deviceId === entity_.device_id) {
+              // homeyDevices[i].matchedEntities.push({
+              // name: entity_.entity_id,
+              // });
+              homeyDevices[i].matchedEntities.push(entity_.entity_id);
+            }
+          });
+          const lengthOfEntities = Object.keys(homeyDevices[i].matchedEntities).length;
+          console.log(lengthOfEntities);
+          entityKeys.forEach(id => {
+            
+          });
+          
+          // match the entities subclass stuff to this
+          
+          i++;
+        }
+        // hier zitten alle entities gekoppeld aan de devices
+
       }
     });
   });
 }
 
 test();
-console.log('devices compatible with Homey:', devArray);
-//console.log(devArray);
+for (let i = 0; i < homeyDevices.length; i++) {
+  homeyDevices[i].display();
+}
+//console.log(homeyDevices);
+
+
+// -- WORKS WITH homeyDevices[i].matchedEntities.push(entity_.entity_id)
+/* for(let k = 0; k < homeyDevices[i].matchedEntities.length; k ++){
+            entityKeys.forEach(id => {
+              if(homeyDevices[i].matchedEntities[k] === id) {
+                homeyDevices[i].subEntities.push({
+                  friendly: file3[id].attributes.friendly_name
+                })
+              }
+            })
+          }
+          */
