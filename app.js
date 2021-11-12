8f99b356f29f45842f2a5620bad5bb35b1e15ca8
MarleenBee <marleenbeekman@gmail.com>
2021-11-12 13:15:57 +0100

    changed commented IP address

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Homey = require('homey');
 const HAClient = require('./lib/HAClient');
 
 // TODO: Make dynamic from ManagerSettings
-const address = 'http://homeassistant.local:8123'; //http://192.168.0.73:8123
+const address = 'http://homeassistant.local:8123'; //http://192.168.0.84:8123
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJlYTIzMDk5YTMyMjE0MGMzOTFlMzUxYWRmNGY0NjVhOSIsImlhdCI6MTYzMjgxMDg1NSwiZXhwIjoxOTQ4MTcwODU1fQ.NHBA_n47UO_7JZu7DU7El7tCBkYR07LLFHPYhtMJNko';
 module.exports = class HomeAssistantApp extends Homey.App {
 
