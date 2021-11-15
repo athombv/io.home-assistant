1eefb9843d57daea584181eb9582e7e27af3012e
MarleenBee <marleenbeekman@gmail.com>
2021-11-15 13:29:11 +0100

    Added debugging comments and updated TODO

--- drivers/homeassistantdevice/driver.js
+++ drivers/homeassistantdevice/driver.js
@@ -12,7 +12,8 @@ module.exports = class HomeAssistantDriver extends Homey.Driver {
     const deviceRegistry = await connection.sendMessagePromise({ type: "config/device_registry/list" });
     const entityRegistry = await connection.sendMessagePromise({ type: "config/entity_registry/list" });
     const entities = await client.getEntities();
-
+    //console.log(JSON.stringify(deviceRegistry, null, 4));
+    //console.log(JSON.stringify(entityRegistry, null, 4));
     return deviceRegistry
       .map(device => {
         return {
@@ -30,7 +31,7 @@ module.exports = class HomeAssistantDriver extends Homey.Driver {
       .map(device => {
         const deviceClass = 'other';
         const deviceCapabilities = [];
-        const deviceStore = {};
+        const deviceStore = {}; // capabilities: "", 
 
         device.entities.forEach(({ entity_id: entityId }) => {
           const entity = entities[entityId];
@@ -38,13 +39,12 @@ module.exports = class HomeAssistantDriver extends Homey.Driver {
 
           const capabilityId = HAUtil.getCapabilityFromEntity(entity);
           if (!capabilityId) return;
-
+          // something is wrong here it executes the devicestore stuff as many times as that there are entities connected to said device -- ? possible issue, keep tracking it
           deviceCapabilities.push(capabilityId);
           deviceStore.capabilities = deviceStore.capabilities || {};
           deviceStore.capabilities[capabilityId] = { entityId };
         });
-
-        // TODO: deviceClass like blinds, windows, sensors etc..
+        // TODO: deviceClass like blinds, windows, sensors etc.
 
         return {
           class: deviceClass,
@@ -56,6 +56,7 @@ module.exports = class HomeAssistantDriver extends Homey.Driver {
           },
         }
       })
+      // the below filter is the issue, when the code doesnt recognize any capabilities (what i had with the light) it wont show it in onPairlistDevices
       .filter(device => {
         if (device.capabilities.length === 0) return false;
         return true;
