95008953fc2affdd25b10dec9f8c142f07a22b50
MarleenBee <marleenbeekman@gmail.com>
2021-11-19 16:59:03 +0100

    Added event listener, removed entityUpdate()

--- drivers/homeassistantdevice/device.js
+++ drivers/homeassistantdevice/device.js
@@ -2,7 +2,6 @@
 
 const Homey = require('homey');
 const HAUtil = require('../../lib/HAUtil');
-
 class MyDevice extends Homey.Device {
   async onInit() {
     this.log('MyDevice has been initialized: ');
@@ -10,10 +9,9 @@ class MyDevice extends Homey.Device {
     this.client = this.homey.app.getClient();
     this.deviceId = this.getData().id;
     this.capabilities = this.getCapabilities();
+    //this.entities = this.entities.prototype(this);
+    this.store = this.getStore();
     this.capabilities.forEach(capabilityId => {
-      console.log(capabilityId);
-      // TODO: Register Capability Listener
-      // Missing Capability Listener: light_temperature
       // TODO: Register Realtime Entity Update
       // the function from the websocket will listen to all events when they say 'state_changed', but you need to filter out when the entity_id of said changed state is IN the 'getStore()' then update capabilities
       // i said it like this because then not only will it work for the lights, but also for the weather sensor.
@@ -28,26 +26,54 @@ class MyDevice extends Homey.Device {
           data.brightness_pct = value * 100;
         }
         // homey value: between 1 and 0
-        // ha value: 250 ( cold) en 454 (warm)
+        // ha value: 250 (cold) en 454 (warm)
         if (capabilityId == 'light_temperature') {
-          data.color_temp = 204 * value + 250
+          data.color_temp = 204 * value + 250;
+        }
+        // TODO register mutiple capability listener
+        if (capabilityId == 'light_hue') {
+          data.hs_color = 360 * value;
+        }
+        if (capabilityId == 'light_saturation') {
+          data.hs_color = 100 * value;
         }
         this.log(data);
+
         this.client.updateLight(value, data);
       });
-      //TODO Make Light dimmable (e14 or the e27), they both work with 'onoff'
     });
 
-    this.log('device initialized');
-    this.log('id: ', this.deviceId);
-    this.log('name: ', this.getName());
-    this.log('class: ', this.getClass());
-    this.log('capabilities: ', this.capabilities);
-    this.log('store: ', this.getStore());
-
-    const entity = this.client.getEntities(this.entityId);
+    const entity = this.client.getEntities();
+    // returns promise
     if (entity) {
-      this.entityUpdate(entity);
+      this.client.once('listedEntities', (entities) => {
+        this.entities = entities;
+        return this.entities;
+      });
+      this.client.on('state_changed', (data) => {
+        if (this.store.deviceEntities[data.entity_id]) {
+          this.capabilities.forEach(capabilityId => {
+            if (this.store.deviceEntities[data.entity_id].capabilityId == capabilityId) { //pastes the new_state value in the correct capability
+              if (data.new_state.state == 'on') {
+                this.setCapabilityValue(capabilityId, true);
+              } else if (data.new_state.state == 'off') {
+                this.setCapabilityValue(capabilityId, false);
+              } else {
+                this.setCapabilityValue(capabilityId, parseFloat(data.new_state.state));
+              }
+            }
+            if (this.getCapabilityValue(capabilityId) == null) {
+              Object.keys(this.entities).forEach(key => {
+                if (this.store.deviceEntities[key]) {
+                  if (capabilityId == this.store.deviceEntities[key].capabilityId) {
+                    this.setCapabilityValue(capabilityId, parseFloat(this.entities[key].state));
+                  }
+                }
+              })
+            }
+          })
+        }
+      })
     }
   }
 
@@ -60,17 +86,6 @@ class MyDevice extends Homey.Device {
 
   async onDeleted() {
     this.log('MyDevice has been deleted:', this.getName());
-  }
-  //TODO make async
-  async entityUpdate(data) {
-    console.log('updating capabilities');
-    try {
-      this.capabilities.forEach(capabilityId => {
-        this.setCapabilityValue(capabilityId, parseFloat(data.state)); //dont set other capabilities to null   
-      });
-    } catch (ex) {
-      console.log('error', ex);
-    }
   }
 }
 
@@ -87,3 +102,7 @@ issue: rgb light isnt working
 
 classes: dan weet homey ook of het een lamp is of niet
 */
+/*
+    this.client.on('state_changed', (data) => {
+      console.log('data:', data);
+    });*/
