1fdc2256019ed3af3b67e486a743c0a004f9fa54
MarleenBee <marleenbeekman@gmail.com>
2021-11-29 17:27:56 +0100

    Removed debugging log

--- drivers/homeassistantdevice/driver.js
+++ drivers/homeassistantdevice/driver.js
@@ -12,7 +12,7 @@ module.exports = class HomeAssistantDriver extends Homey.Driver {
     const deviceRegistry = await connection.sendMessagePromise({ type: "config/device_registry/list" });
     const entityRegistry = await connection.sendMessagePromise({ type: "config/entity_registry/list" });
     const entities = await client.getEntities();
-    //console.log(JSON.stringify(deviceRegistry, null, 4));
+    console.log(JSON.stringify(deviceRegistry, null, 4));
     //console.log(JSON.stringify(entityRegistry, null, 4));
     return deviceRegistry
       .map(device => {
@@ -32,24 +32,26 @@ module.exports = class HomeAssistantDriver extends Homey.Driver {
         const deviceClass = 'other';
         let deviceEntities = [];
         const deviceStore = {}; // capabilities: "", 
-
         device.entities.forEach(({ entity_id: entityId }) => {
+          console.log("Entity ID: ", entityId);
           const entity = entities[entityId];
+          //console.log("entity:", entity);
           if (!entity) return;
-
+          console.log("test");
           //TODO make 1 entity have 1 capability, if it needs more capabilities, duplicate entity
           // if capability length != 1 -> getcapabilityfromentity
           const capabilityId = HAUtil.getCapabilityFromEntity(entity);
+          console.log("Capability: ", capabilityId);
+          if (!capabilityId) return;
           // the light is 1 device with mutiple entities, already making capabilityId an entire array. when you want to push the capabilityId in deviceCapabilities, you get an array inside of an array
           // something is wrong here it executes the devicestore stuff as many times as that there are entities connected to said device -- ? possible issue, keep tracking it
-          if (Array.isArray(capabilityId)) {
-            deviceEntities = deviceEntities.concat(capabilityId);
-          }
+          deviceEntities = deviceEntities.concat(capabilityId);
           deviceStore.deviceEntities = deviceStore.deviceEntities || {};
           deviceStore.deviceEntities[entityId] = { capabilityId };
         });
         // TODO: deviceClass like blinds, windows, sensors etc.
-
+        console.log("stored:", deviceStore);
+        console.log("data: ", device.id);
         return {
           class: deviceClass,
           capabilities: deviceEntities,
